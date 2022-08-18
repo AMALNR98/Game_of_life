1b0b5a759977f5e886d73cd9031a8505551dfa78
AMALNR98 <amalnr123@gmail.com>
2022-08-18 10:17:51 +0530

    test for neighboring cells created

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -10,7 +10,16 @@ def test_rules_for_gameoflife():
               [0,0,1,0,0],
               [0,0,1,0,0],
               [0,0,1,0,0],
-              [0,0,1,0,1]]
+              [0,0,0,0,0]]
 
     assert game_of_life.rules_for_gameoflife(game_matrix)== result
-    
+
+def test_for_nigbours_cell():
+    game_matrix = [[0,1,0],
+                   [1,0,1],
+                   [0,1,0]]
+    assert game_of_life.neighbouring_cells(game_matrix,1,1)==4
+    assert game_of_life.neighbouring_cells(game_matrix,0,1)==2
+
+
+
