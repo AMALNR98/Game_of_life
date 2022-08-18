0dd9e9d6aa7fe562f0cc89f4b670c89e2b4fb05f
AMALNR98 <amalnr123@gmail.com>
2022-08-18 11:14:23 +0530

    added one more test case for display(for coverage)

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,3 +1,4 @@
+from unittest import result
 import game_of_life
 
 def test_rules_gameoflife():
@@ -14,12 +15,18 @@ def test_rules_gameoflife():
 
     assert game_of_life.rules_gameoflife(game_matrix)== result
 
-def test_for_nigbours_cell():
+def test_nigbours_cell():
     game_matrix = [[0,1,0],
                    [1,0,1],
                    [0,1,0]]
     assert game_of_life.neighboring_cells(game_matrix,1,1)==4
     assert game_of_life.neighboring_cells(game_matrix,0,1)==2
 
-
+def test_display():
+    game_matrix = [[0,1,0],
+                   [1,0,1],
+                   [0,1,0]]
+    
+    result = '0\t1\t0\n\n\n1\t0\t1\n\n\n0\t1\t0'
+    assert game_of_life.display(game_matrix) == result
 
